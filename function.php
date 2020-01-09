672b94e96c57db6d637cb9f62c7c64426aa4a40b
Seb-dupuis <sebastien-dupuis@laposte.net>
2020-01-09 15:45:40 +0100

    change PATH style.css

--- function.php
+++ function.php
@@ -10,6 +10,6 @@
  */
 
 /* file d'attente des scripts et des styles*/
-wp_enqueue_style( 'keenetnoa-style', get_stylesheet_uri() );
+wp_enqueue_style( 'style', get_stylesheet_uri() );
 
 wp_enqueue_script( 'script', get_template_directory_uri(), true);
