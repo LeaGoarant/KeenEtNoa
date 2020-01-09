9a3b0f96b8118bfe76021b08d4726a0ef459ccf8
Seb-dupuis <sebastien-dupuis@laposte.net>
2020-01-09 14:27:16 +0100

    change head

--- header.php
+++ header.php
@@ -1,53 +1,57 @@
-<?php 
+<?php
 /** The header is for our theme
  * This is the template that displays all of the <head> section and everything up until <div id="content">
- * 
+ *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- * 
+ *
  * @package WordPress
  * @subpackage KeenEtNoa
  * @since 1.0.0
  */
+
 ?>
 
 <!DOCTYPE HTML>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes();?>>
 
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta charset="<?php bloginfo('charset');?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<?php wp_head(); ?>
-    <link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head();?>
+    <link href="<?php echo get_bloginfo('template_directory'); ?>/style.css" rel="steelsheet">
+    <title><?php echo get_bloginfo('name'); ?></title>
+    <?php wp_head();?>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class();?>>
     <div id="page" class="site">
-        <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'KeenEtNoa' ); ?></a>
-
+        <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'KeenEtNoa');?></a>
+    <h1 class="blog-title" ><?php echo get_bloginfo('name'); ?></h1>
+    <p>
         <header id="masthead" class="site-header">
         <div class="container header-container">
 			<div class="navbar-header">
 				<div class="site-branding">
-					<?php keen_et_noa_brand(); ?>
+					<?php keen_et_noa_brand();?>
 				</div><!-- .site-branding -->
              </div>
 
             <nav id="site-navigation" class="main-navigation site-navigation">
                 <?php
-                wp_nav_menu(
-                    array(
-                        'theme_location' => 'primary',
-                        'menu_id'        => 'primary-menu',
-                        'depth'          => 2,
-                    )
-                );
-                    ?>
+wp_nav_menu(
+    array(
+        'theme_location' => 'primary',
+        'menu_id' => 'primary-menu',
+        'depth' => 2,
+    )
+);
+?>
             </nav><!-- #site-navigation -->
         </div>
 
         </header><!--masthead-->
 
-        <?php keen_et_noa_categories_menu(); ?>
+        <?php keen_et_noa_categories_menu();?>
 
 		<div id="content" class="site-content">
 
