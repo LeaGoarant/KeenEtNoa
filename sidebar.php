7c2aae24c8a1a466816f0a667026ff7a1773c8ff
Seb-dupuis <sebastien-dupuis@laposte.net>
2020-01-10 10:10:17 +0100

    indentation

--- sidebar.php
+++ sidebar.php
@@ -12,7 +12,7 @@
   <div class="sidebar-module">
     <h4>Elsewhere</h4>
     <ol class="list-unstyled">
-    <li><a href="<?php echo get_option('twitter'); ?>">Twitter</a></li>
+      <li><a href="<?php echo get_option('twitter'); ?>">Twitter</a></li>
       <li><a href="#">Facebook</a></li>
     </ol>
   </div>
