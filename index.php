1797f3f704f50b3b9993a6f306be5ef78354280a
Seb-dupuis <sebastien-dupuis@laposte.net>
2020-01-13 16:24:25 +0100

    panel admin add image

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <div class="row">
 	<div class="col-sm-8 blog-main">
 
-
+	<a href="<?php echo esc_url( $wptuts_options['logo'] ); ?>"><img src="<?php echo esc_url( $wptuts_options['logo'] ); ?>" alt="" title="" width="" height="" class="alignzone size-full wp-image-125" /></a>	
 
 
 
