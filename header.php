ab42531f9e20f8b50e8fd93dc94c596aacd63c5c
Lea Goarant <leagoarant@msn.com>
2020-01-15 14:56:24 +0100

    modifying search bar

--- header.php
+++ header.php
@@ -25,8 +25,12 @@
 <body <?php body_class(); ?>>
 
     <header id="masthead" class="site-header">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="<?php bloginfo('url') ?>"><img class="keen_logo" src="http://127.0.0.1/Wordpress/wp-content/uploads/2020/01/logo-original_small-4.png" /></a>
+
+        <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+
+            <a class="navbar-brand" href="<?php bloginfo('url') ?>">
+                <img class="keen_logo" src="http://127.0.0.1/Wordpress/wp-content/uploads/2020/01/logo-original_small-4.png" />
+            </a>
 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -45,11 +49,18 @@
                     )
                 );
                 ?>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+
+            <button class="btn btn-outline" type="submit" id="button-addon2" data-toggle="collapse" data-target="#searchbar">
+                <i class="fas fa-search"></i>
+            </button>
+            <div class="collapse" id="searchbar">
+                <form class="form-inline ml-auto">
+                    <div class="md-form my-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Rechercher" aria-label="Search">
+                    </div>
                 </form>
             </div>
+            
         </nav>
     </header>
     <!--masthead-->
