2ecbe30cfe305e1d8e433b2835f59ae303f8d68f
Lea Goarant <leagoarant@msn.com>
2020-01-17 11:01:58 +0100

    continue sales conditions

--- PageTemplater/salescondition-template.php
+++ PageTemplater/salescondition-template.php
@@ -140,20 +140,131 @@
                 <br/>
                 La société www.keenetnoa.fr propose la souscription d’un abonnement mensuel, ou d’un abonnement à engagement minimum de trois (3) mois, ou d’un abonnement à engagement minimum de six (6) mois à la suite duquel l’abonnement se poursuit sans nouvelle période minimum d’engagement par tacite reconduction, d’un mois, trois mois ou six mois sur l’autre, jusqu’à ce que le client souhaite l’interrompre. Ces formules sont proposées au choix du Client, lors de sa souscription.
                 Tout abonnement est conclu pour sa durée ferme et les sommes versées (y compris pour un bon cadeau) à ce titre sont définitivement acquises à la société Théodore Végétal, y compris en cas de résiliation de l’abonnement avant son terme.
+                <br/>
+                <br/>
+                <strong>Renouvellement automatique de l’abonnement « trois mois » ou « six mois » :</strong>   
+                <br/> 
+                <br/>         
+                Contrairement à l’abonnement mensuel, le Client est informé qu’à son échéance l’abonnement « trimestriel » ou « semestriel » sera automatiquement reconduit pour une durée identique de trois ou six mois, sauf résiliation par le Client au plus tard 5 jours (ouvrés) avant l’expiration de l’échéance de l’abonnement « trimestriel » ou « semestriel » en cours.
+                <br/>
+                <br/>
+                <strong>Résiliation de l’abonnement :</strong>
+                <br/>
+                <br/>
+                Le Client peut mettre fin à son abonnement « trimestriel » ou « semestriel » à l’issue d’une période minimale d’un (1) mois. Le Client souhaitant mettre fin à son abonnement doit en avertir la société Théodore Végétal, cinq (5) jours ouvrés avant le dernier jour du trimestre ou semestre en cours (selon la formule choisie), en envoyant un courrier ou un e-mail aux adresses suivantes :
+                <br/>
+                <br/>
+                SASU Théodore Végétal
+                <br/>
+                30 rue George Sand à Reims (51100)
+                <br/>
+                theodoreveg@gmail.com
+                <br/>
+                <br/>
+                L’abonnement « trimestriel » ou « semestriel » prendra alors fin au dernier jour du mois en cours.
+                En l’absence de résiliation de son abonnement « trimestriel » ou « semestriel », le Client est informé qu’un prélèvement aura lieu le mois suivant. 
+                Pour tout abonnement souscrit avant le dernier jour du mois en cours, le Client recevra les produits sélectionnés par la société Théodore Végétal, le mois suivant de la souscription, conformément à ce qui est présenté sur la page de vente.
+                Tout Client a la possibilité d’acheter par l’intermédiaire du Site, les Produits découverts grâce à son abonnement le mois suivant, selon disponibilité des offres proposées sur le Site. 
+                <br/>
+                <br/>
+                <strong>Modification de l’Abonnement :</strong>
+                <br/>
+                <br/>
+                Les abonnements à durée trois (3) mois et six (6) mois ne sont pas modifiables, outre l’adresse de livraison, de facturation et le mode de livraison.
+                Lors de la souscription d’un abonnement, il appartient au Client de vérifier que l’abonnement est conforme à ses choix (Données Personnelles, mode de livraison, options éventuelles, formule ou catégorie d’abonnement, lieu de livraison et/ou de facturation des produits, etc.).
+                En cas de résiliation par le Client d’un abonnement « 3 mois » ou « 6 mois », les éventuels avoirs obtenus devront avoir été utilisés à défaut de quoi ils pourront être considérés comme perdus.
+                La société Théodore Végétal se réserve par ailleurs expressément le droit d’étudier au cas par cas et d’accepter ou de refuser toute souscription à un abonnement pour laquelle les livraisons doivent avoir lieu hors de France, notamment au regard des règles et conditions particulières applicables dans le pays concerné, ce que le Client reconnaît et accepte expressément. En cas de refus de souscription à un abonnement dans ce cadre, la société Théodore Végétal en informera le Client dans les plus brefs délais et, le cas échéant, le remboursera du prix déjà payé et prélevé.
+                Le Client a la possibilité de vérifier le détail de sa commande, son prix total et de corriger d’éventuelles erreurs avant de confirmer son acceptation. Il lui incombe de vérifier l’exactitude de la commande et de signaler ou rectifier immédiatement toute erreur.
+                <br/>
+                <br/>
+                <strong>Les commandes de produits dans la boutique :</strong>
+                <br/>
+                <br/>
+                <ul>
+                    <strong><li>- Sélection des produits dans la boutique</li></strong>
+                    <br/>
+                    Il appartient au Client de sélectionner sur le site internet www.keenetnoa.fr les produits qu’il désire commander, selon les modalités suivantes : 
+                    <br/>
+                    <br/>
+                    <strong><li>- Choix des produits et quantités désirées</li></strong>
+                    <br/>
+                    La vente n’est définitive qu’après l’envoi au Client de la confirmation de l’acceptation de la commande par le Vendeur par courrier électronique, qui doit être envoyé sans délai et après encaissement par celui-ci de l’intégralité du prix.
+                    Toute commande passée, validée par le Client et confirmée par le Vendeur, dans les conditions et selon les modalités ci-dessus décrites, sur le site Internet www.keenetnoa.fr constitue la formation d’un contrat conclu à distance entre le Client et le Vendeur.
+                    Sauf preuve contraire, les données enregistrées dans le système informatique du Vendeur constituent la preuve de l’ensemble des transactions conclues avec le Client.
+                    Le Vendeur se réserve le droit d’annuler ou de refuser toute commande d’un Client avec lequel il existerait un litige relatif au paiement d’une commande antérieure.
+                    Le Client pourra suivre l’évolution de sa commande sur le site internet www.keenetnoa.fr.
+                    Le Vendeur n’a pas vocation à vendre les produits sur le site internet www.keenetnoa.fr à des professionnels, mais uniquement à des consommateurs ou à des non professionnels, pour leurs besoins personnels.
+                    Le Vendeur se réserve donc le droit de refuser les commandes d’un même Produit en quantités importantes et comportant plus de www.keenetnoa.fr.
+                    Aucune commande d’un montant inférieur à 20 euros HT (pour la France et Europe) et 30 euros HT (hors Europe) ne pourra être acceptée.
+                </ul>
+                <br/>
+                <br/>
+                <strong>5-2 . Modification de la commande</strong>
+                <br/>
+                <br/>
+                Une fois confirmée et acceptée par le Vendeur, dans les conditions ci-dessus décrites, la commande n’est pas modifiable.
+                <br/>
+                <br/>
+                <strong>5-3 . Annulation de la commande</strong>
+                <br/>
+                <br/>
+                Une fois confirmée et acceptée par le Vendeur, dans les conditions ci-dessus décrites, la commande ne peut être annulée, hors l’exercice du droit de rétractation ou cas de force majeure.
+                En cas d’annulation de la commande par le Client après son acceptation par le Vendeur moins de cinq (5) jours au moins avant la date prévue pour la fourniture des Produits commandés, pour quelque raison que ce soit hormis l’exercice du droit de rétractation ou la force majeure, une somme correspondant à 50 % du montant total de l’achat sera acquise au Vendeur et facturée au Client, à titre de dommages et intérêts, en réparation du préjudice ainsi subi.
             </p>
             <br/>
 
-            <h3></h3>
+            <h3>ARTICLE 6 – Tarifs</h3>
             <br/>
-            <p></p>
+            <p>
+            Les Produits sont fournis aux tarifs en vigueur figurant sur le site Internet www.keenetnoa.fr, lors de l’enregistrement de la commande par le Vendeur. Les prix sont exprimés en Euros, HT et TTC.
+            Les tarifs tiennent compte d’éventuelles réductions qui seraient consenties par le Vendeur sur le site internet www.keenetnoa.fr.
+            Ces tarifs sont fermes et non révisables pendant leur période de validité, telle qu’indiqué sur le site internet www.keenetnoa.fr, le Vendeur se réservant le droit, hors cette période de validité, de modifier les prix à tout moment. Ils ne comprennent pas les frais de traitement, d’expédition, de transport et de livraison, qui sont facturés en supplément, dans les conditions indiquées sur le site internet www.keenetnoa.fr et calculés préalablement à la passation de la commande.
+            Si le Client demande un mode d’expédition plus rapide ou plus coûteux que l’expédition standard, les frais supplémentaires de livraison, tels qu’ils apparaissent au moment de la validation de la commande par le Client, sont intégralement à sa charge.
+            Le paiement demandé au Client correspond au montant total de l’achat, y compris ces frais.
+            </p>
             <br/>
 
-            <h3></h3>
+            <h3>ARTICLE 7 – Conditions de paiement</h3>
             <br/>
-            <p></p>
+            <p>
+            Le prix est payable comptant, en totalité au jour de la passation de la commande par le Client.
+            L’expédition de la commande par le Vendeur la livraison des Produits commandés, par voie de paiement sécurisé, selon les modalités suivantes : 
+            <ul>
+                <li>- par cartes bancaires : Carte Bancaire, Visa, MasterCard, American Express, autres cartes bancaires</li>
+                <li>- par téléphone</li>
+                <li>- par porte-monnaie électronique</li>
+                <li>- par chèque bancaire, pour toute commande supérieure ou égale à 500 euros TTC</li>
+                <li>- par virement bancaire</li>
+            </ul>
+            <br/>
+            Le paiement par carte bancaire est irrévocable, sauf en cas d’utilisation frauduleuse de la carte. Dans ce cas, le Client peut demander l’annulation du paiement et la restitution des sommes correspondantes.
+            En cas de paiement par chèque bancaire, celui-ci doit être émis par une banque domiciliée en France métropolitaine ou à Monaco.
+            La mise à l’encaissement du chèque est réalisée à réception.
+            Les données de paiement sont transmises de manière sécurisée via une connexion HTTPS et HSTS. Le protocole de chiffrement utilisé est AES-256.
+            Le prix est payable en totalité et en un seul versement à compter de la commande, telle que définie à l’article «Livraisons» ci-après comme précisé sur la facture adressée au Client par voie de paiement sécurisé, selon les modalités suivantes : 
+            <br/>
+            <br/>
+            <ul>
+                <li>- par cartes bancaires : Carte Bancaire, Visa, MasterCard, American Express, autres cartes bancaires</li>
+                <li>- par téléphone</li>
+                <li>- par porte-monnaie électronique</li>
+                <li>- par chèque bancaire, pour toute commande supérieure ou égale à 500 euros TTC</li>
+                <li>- par virement bancaire</li>
+            </ul>
+            Le paiement par carte bancaire est irrévocable, sauf en cas d’utilisation frauduleuse de la carte. Dans ce cas, le Client peut demander l’annulation du paiement et la restitution des sommes correspondantes.
+            En cas de paiement par chèque bancaire, celui-ci doit être émis par une banque domiciliée en France métropolitaine ou à Monaco.
+            La mise à l’encaissement du chèque est réalisée à réception.
+            Les données de paiement sont transmises de manière sécurisée via une connexion HTTPS et HSTS. Le protocole de chiffrement utilisé est AES-256.
+            En cas de retard de paiement et de versement des sommes dues par le Client au-delà des délais ci-dessus fixés, et après la date de paiement figurant sur la facture adressée à celui-ci, des pénalités de retard calculées au taux mensuel de 7% du montant TTC du prix d’acquisition, majoré des frais d’expédition, figurant sur ladite facture, seront acquises automatiquement et de plein droit au Vendeur, sans formalité aucune ni mise en demeure préalable.
+            En outre, tout retard de paiement entraine de plein droit l’application d’une indemnité forfaitaire de quarante (40) Euros, sans préjudice des pénalités de retard.
+            Le retard de paiement entraînera également l’exigibilité immédiate de l’intégralité des sommes dues par le Client, sans préjudice de toute autre action que le Vendeur serait en droit d’intenter, à ce titre, à l’encontre du Client.
+            Les paiements effectués par le Client ne seront considérés comme définitifs qu’après encaissement effectif des sommes dues, par le Vendeur.
+            En outre, le Vendeur se réserve le droit, en cas de non-respect des conditions de paiement figurant ci-dessus, de suspendre ou d’annuler la livraison des commandes en cours effectuées par le Client.
+            Aucun frais supplémentaire, supérieur aux coûts supportés par le Vendeur pour l’utilisation d’un moyen de paiement ne pourra être facturé au Client.
+            </p>
             <br/>
 
-            <h3></h3>
+            <h3>ARTICLE 8 – Livraisons</h3>
             <br/>
             <p></p>
             <br/>
