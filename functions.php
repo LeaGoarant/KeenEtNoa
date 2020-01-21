da71b4c801c26bc124c49ebe690ccd9848845992
Lea Goarant <leagoarant@msn.com>
2020-01-21 17:23:05 +0100

    modifying single page product

--- functions.php
+++ functions.php
@@ -82,3 +82,6 @@ remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 20);
 remove_action('woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30);
 remove_action('woocommerce_before_shop_loop', 'woocommerce_result_count', 20);
 
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 10);
+add_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 30);
