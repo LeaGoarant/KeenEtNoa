e21ee383301a01b3baf7a83062b30e1355873548
Lea Goarant <leagoarant@msn.com>
2020-01-22 11:32:38 +0100

    adding function to give max size to description

--- functions.php
+++ functions.php
@@ -84,4 +84,4 @@ remove_action('woocommerce_before_shop_loop', 'woocommerce_result_count', 20);
 
 remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
 remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 10);
-add_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 30);
+add_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 25);
