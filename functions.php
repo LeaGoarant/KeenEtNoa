06ddbbf1ea830f1d0a662124aea0047f5d7705a0
Lea Goarant <leagoarant@msn.com>
2020-02-04 12:36:09 +0100

    adding functional read more

--- functions.php
+++ functions.php
@@ -102,3 +102,43 @@ function addBootstrapToCheckoutFields($fields)
 
 /* Nav Profil Page */
 remove_action('woocommerce_account_navigation', 'woocommerce_account_navigation');
+
+
+/**
+ * @snippet       Truncate Short Description @ WooCommerce Single
+ * @how-to        Get CustomizeWoo.com FREE
+ * @author        Rodolfo Melogli
+ * @compatible    WooCommerce 3.7
+ * @donate $9     https://businessbloomer.com/bloomer-armada/
+ */
+ 
+add_action( 'wp_footer', 'bbloomer_woocommerce_short_description_truncate_read_more' );
+ 
+function bbloomer_woocommerce_short_description_truncate_read_more() { 
+   if ( ! is_product() ) return;
+   ?>
+   <script type="text/javascript">
+       
+         jQuery(document).ready(function($){
+          
+         var show_char = 800;
+         var ellipses = "... ";
+         var content = $(".woocommerce-product-details__short-description").html();
+          
+         if (content.length > show_char) {
+            var a = content.substr(0, show_char);
+            var b = content.substr(show_char - content.length);
+            var html = a + '<span class="truncated">' + ellipses + '<a href="" class="read-more">Read more</a></span><span class="truncated" style="display:none">' + b + '</span>';
+            $(".woocommerce-product-details__short-description").html(html);
+         }
+ 
+         $(".read-more").click(function(e) {
+            e.preventDefault();
+            $(".woocommerce-product-details__short-description .truncated").toggle();
+         });
+ 
+      });
+       
+   </script>
+   <?php
+}
