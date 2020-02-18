f5da85e50357a159562d4e2d4074fae5e90a7fb3
Lea Goarant <leagoarant@msn.com>
2020-02-18 10:28:30 +0100

    amodifying images

--- PageTemplater/who-template.php
+++ PageTemplater/who-template.php
@@ -13,14 +13,18 @@
         <div class="card-body">
             <h2>Qui sommes-nous ?</h2>
             <p>
-            Mai 2019, le fondateur Alex prend une décision d’arrêter son emploi, il n’avait aucune autre proposition en vue mais il devait arrêter ce qu’il faisait jusqu’alors, parce qu’il avait un rêve, être maître de sa vie.
+            Mai 2019, le fondateur Jean-Michel prend la décision d’arrêter son emploi, il n’avait aucune autre proposition en vue mais il devait arrêter ce qu’il faisait jusqu’alors, parce qu’il avait un rêve, être maître de sa vie.
 ​
-Ressentir le bien-être que procure une alimentation vegan, et se sentir bien malgré une vie, un environnement très agité qui ne lui correspondait pas, il voulait changer et transmettre son expérience à d’autres.
+Ressentir le bien-être que procure une alimentation vegan, et se sentir bien malgré une vie et un 
+environnement très agité qui ne lui correspondait pas. Voilà ce qu'il voulait partager et transmettre. 
 ​
-Comme lui, et ces amis Vegan, il avait des difficultés pour s’alimenter et trouver des produits de qualité et gustativement satisfaisants.
-La solution apparait de manière évidente : créer son site de vente de produits végan. 
+Comme lui, et ces amis vegans, il avait des difficultés pour s’alimenter et trouver des produits de qualité et gustativement satisfaisants.
+La solution apparait de manière évidente : créer son site de vente de produits vegan. 
 ​
-Il commence tout simplement par créer sa marque Keen et Noa, puis son site internet pour proposer des produits « vegan’ment meilleurs » qu’il sélectionne auprès de fournisseurs du monde entier pour remplacer les produits quotidiens essentiels comme les snacks, les vins, les “Vromages”, des produits de base en alimentaire, des pâtes, farines, le chocolat (cru), …sans pour autant renoncer au goût, à la gourmandise tout en veillant au respect des animaux et de la vie en général. 
+Il commence tout simplement par créer sa marque Keen Et Noa, puis son site internet pour proposer des produits « vegan’ment meilleurs » qu’il sélectionne auprès 
+de fournisseurs du monde entier pour remplacer les produits quotidiens essentiels comme les snacks, les vins, les “vromages”, des produits de base en 
+alimentaire, des pâtes, farines, le chocolat (cru), sans pour autant renoncer au goût, à la gourmandise tout en veillant au
+respect des animaux et de la vie en général. 
 ​
 L’histoire avait commencé…
 
